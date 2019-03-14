e0ef898a207c788f01ddeddc945e0fc5a39754c6
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-03-13 21:02:35 -0400

    features with linearity

--- feature_relationships.py
+++ feature_relationships.py
@@ -1,6 +1,9 @@
 from pandas import *
 
+
 df = read_csv('data/clean_training_data.csv', sep=',', header=0, index_col='date')
 
 # Calculates pearson correlation coefficient between all values
 print(df.corr()[['mean_temp']].sort_values('mean_temp'))
+
+# plt = df.plot.scatter("mean_temp", "mean_temp_2")
