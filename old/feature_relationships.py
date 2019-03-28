aecee4ecdabf21933b13ff164840d391cfecc784
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-03-28 11:44:01 -0400

    remove old

--- old/feature_relationships.py
+++
@@ -1,8 +0,0 @@
-from pandas import *
-
-df = read_csv('data/clean_training_data_year.csv', sep=',', header=0, index_col='date')
-relations = df.corr()[['mean_temp']].sort_values('mean_temp')
-relations.to_csv('data/relationships.csv', sep=',')
-# Calculates pearson correlation coefficient between all values
-print(relations)
-
