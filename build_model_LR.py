c668b3c87650e85f065a6b8df9618259e61aa1d3
Anirudh Mungre <anirudh.mungre16@gmail.com>
2019-03-31 16:44:41 -0400

    build model

--- build_model_LR.py
+++ build_model_LR.py
@@ -11,10 +11,11 @@ X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
 model = LinearRegression()
 model.fit(X_train, Y_train)
+print(X_test)
 # X_test = read_csv('./training_data/march24_test_dewpt.csv', sep=',', header=0, index_col=['Date/Time', 'Year', 'Month', 'Day', 'Time'])
 prediction = model.predict(X_test)
 # print(prediction)
-dump(model, './models/scikit/temp_model.joblib')
+dump(model, './models/temp_model.joblib')
 
 print(f'The Explained Variance: {model.score(X_test, Y_test)}')
 print(f'The Mean Absolute Error: {mean_absolute_error(Y_test, prediction)} degrees celsius')
