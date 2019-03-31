b169108df5f9baa7d827763da4f2d1750846b297
Ethan Elliott <ethan.elliott@hotmail.ca>
2019-03-30 20:23:27 -0400

    adding the front

---
+++ rain_front/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app dark>
+    <v-toolbar app color="primary">
+      <v-toolbar-title class="headline">
+        <span class="font-weight-light">r</span>
+        <span>AI</span>
+        <span class="font-weight-light">n</span>
+      </v-toolbar-title>
+    </v-toolbar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
